980cc494baea60fda1914968233e4d04abc2d746
jj916 <jhonjairo.916@hotmail.com>
2017-10-27 02:08:08 +0000

    Creating the model Job
    Fixing typo
    
    Adding functionality to Model Job

---
+++ app/Job.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Job extends Model
+{
+    //
+    protected $title;
+    public function __construct($title)
+    {
+    	$this->title=$title;
+    }
+}
